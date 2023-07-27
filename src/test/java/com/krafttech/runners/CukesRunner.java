ab736cc9afa83d527f25a47b987ec96c3ff4a90a
Onur <ozfuttu@gmil.com>
2023-07-27 21:20:16 +0300

    parallel execution class and pom.xml

--- src/test/java/com/krafttech/runners/CukesRunner.java
+++ src/test/java/com/krafttech/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/krafttech/stepDefinitions",
         dryRun = false,
-        tags = "@navigation"
+        tags = "@regression"
 )
 public class CukesRunner {
 
