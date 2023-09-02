7dba89db4decaa17071a8f4dd8c0a52b782f648b
Onur <ozfuttu@gmil.com>
2023-09-02 09:56:28 +0300

    updated pom.xml and utils

--- src/test/java/com/krafttech/utilities/BrowserUtils.java
+++ src/test/java/com/krafttech/utilities/BrowserUtils.java
@@ -8,12 +8,15 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class BrowserUtils {
     /*
@@ -63,7 +66,7 @@ public class BrowserUtils {
     }
 
     /**
-     * return a list of string from a list of elements
+     * return a list of string from a list of web elements
      *
      * @param list of webelements
      * @return list of string
@@ -113,8 +116,8 @@ public class BrowserUtils {
      * @return
      */
     public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeToWaitInSec);
-        //WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+       // WebDriverWait wait = new WebDriverWait(Driver.get(), timeToWaitInSec);
+        WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
@@ -126,8 +129,8 @@ public class BrowserUtils {
      * @return
      */
     public static WebElement waitForVisibility(By locator, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        //    WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+        //WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
+            WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
@@ -139,8 +142,8 @@ public class BrowserUtils {
      * @return
      */
     public static WebElement waitForClickablility(WebElement element, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        //WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+      //  WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
+        WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
@@ -152,8 +155,8 @@ public class BrowserUtils {
      * @return
      */
     public static WebElement waitForClickablility(By locator, int timeout) {
-        WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
-        //WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+     //   WebDriverWait wait = new WebDriverWait(Driver.get(), timeout);
+        WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
@@ -169,8 +172,8 @@ public class BrowserUtils {
             }
         };
         try {
-            //WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
-            WebDriverWait wait = new WebDriverWait(Driver.get(), timeOutInSeconds);
+            WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+          //  WebDriverWait wait = new WebDriverWait(Driver.get(), timeOutInSeconds);
             wait.until(expectation);
         } catch (Throwable error) {
             error.printStackTrace();
@@ -400,10 +403,20 @@ public class BrowserUtils {
      * @param time
      */
     public static void waitForPresenceOfElement(By by, long time) {
-        new WebDriverWait(Driver.get(), time).until(ExpectedConditions.presenceOfElementLocated(by));
-        //WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
+       // new WebDriverWait(Driver.get(), time).until(ExpectedConditions.presenceOfElementLocated(by));
+        WebDriverWait wait = new WebDriverWait(Driver.get(), Duration.ofSeconds(20));
         //wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
-}
+    public static void clearAndSendKeys(WebElement element,String text){
+        element.clear();
+        element.sendKeys(text);
+    }
 
+    public static List<Map<String,String >> getExcelData(String path, String sheetName){
+        ExcelUtil excelUtil=new ExcelUtil(path, sheetName);
+        List<Map<String, String>> dataList = excelUtil.getDataList();
+        return dataList;
+    }
+
+}
